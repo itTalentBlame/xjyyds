3fdc0f067072d675a8a08820dc264a66e78d86d1
bianling <1343881168@qq.com>
2022-07-07 19:19:00 +0800

    完成了列表渲染

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -3,22 +3,34 @@
     <!-- 表格标题区域 -->
     <thead>
       <tr>
-        <th>#</th>
+        <!-- <th>#</th>
         <th>商品名称</th>
         <th>价格</th>
         <th>标签</th>
-        <th>操作</th>
+        <th>操作</th> -->
+        <slot name="thead"></slot>
       </tr>
     </thead>
     <!-- 表格主体区域 -->
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>商品</td>
-        <td>998</td>
-        <td>xxx</td>
-        <td>xxx</td>
-      </tr>
+      <!-- <tr v-for="item in list" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.goods_name }}</td>
+        <td>{{ item.goods_price }}</td>
+        <td>{{ item.tags }}</td>
+        <td>
+          <button class="btn btn-danger btn-sm" style="margin-right: 8px">
+            删除
+          </button>
+          <button
+            class="btn btn-danger btn-sm"
+            style="background: skyblue; border: 1px solid skyblue"
+          >
+            编辑
+          </button>
+        </td>
+      </tr> -->
+      <slot name="tbody" :row="list"></slot>
     </tbody>
   </table>
 </template>
@@ -26,6 +38,11 @@
 <script>
 export default {
   name: "MyTable",
+  props: {
+    list: {
+      type: Array,
+    },
+  },
 };
 </script>
 
--- src/views/MyGoodsList.vue
+++ src/views/MyGoodsList.vue
@@ -1,14 +1,54 @@
 <template>
   <div>
-    <MyTable></MyTable>
+    <MyTable :list="list">
+      <template v-slot:thead>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <template v-slot:tbody="scoped">
+        <tr v-for="item in scoped.row" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.goods_name }}</td>
+          <td>{{ item.goods_price }}</td>
+          <td>{{ item.tags }}</td>
+          <td>
+            <button class="btn btn-danger btn-sm" style="margin-right: 8px">
+              删除
+            </button>
+            <button
+              class="btn btn-danger btn-sm"
+              style="background: skyblue; border: 1px solid skyblue"
+            >
+              编辑
+            </button>
+          </td>
+        </tr>
+      </template>
+    </MyTable>
   </div>
 </template>
 
 <script>
 import MyTable from "../components/MyTable.vue";
 export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
   components: {
     MyTable,
+  },
+  created() {
+    this.$axios({
+      url: "api/goods",
+    }).then((res) => {
+      console.log(res.data.data);
+      this.list = res.data.data;
+    });
   },
 };
 </script>
