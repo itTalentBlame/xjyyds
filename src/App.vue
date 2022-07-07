332b4dba5390387d996eba00ff35a1c8a4ef0aa9
bianling <1343881168@qq.com>
2022-07-07 17:45:35 +0800

    完成了底部切换功能

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,45 @@
 <template>
-  <div></div>
+  <div>
+    <MyHeader title="tabbar"></MyHeader>
+    <div>
+      <MyTable></MyTable>
+    </div>
+    <MyTabBar :list="tabList"></MyTabBar>
+  </div>
 </template>
 
 <script>
+import MyHeader from "./components/MyHeader.vue";
+import MyTabBar from "./components/MyTabBar.vue";
+import MyTable from "./components/MyTable.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      tabList: [
+        {
+          iconText: "icon-shangpinliebiao",
+          text: "商品列表",
+          componentName: "MyGoodsList",
+        },
+        {
+          iconText: "icon-sousuo",
+          text: "商品搜索",
+          componentName: "MyGoodsSearch",
+        },
+        {
+          iconText: "icon-user",
+          text: "我的信息",
+          componentName: "MyUserInfo",
+        },
+      ],
+    };
+  },
+  components: {
+    MyTable,
+    MyTabBar,
+    MyHeader,
+  },
 };
 </script>
 
---
+++ src/components/MyTabBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="my-tab-bar">
+    <div
+      class="tab-item"
+      :class="{ current: isCurrent == item.componentName }"
+      v-for="item in list"
+      :key="item.componentName"
+      @click="isCurrent = item.componentName"
+    >
+      <!-- 图标 -->
+      <span class="iconfont" :class="item.iconText"></span>
+      <!-- 文字 -->
+      <span>{{ item.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isCurrent: "MyGoodsList",
+    };
+  },
+  props: {
+    list: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-tab-bar {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  .tab-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+.current {
+  color: #1d7bff;
+}
+</style>
