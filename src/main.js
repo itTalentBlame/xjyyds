a2ce1ca828b5e9dd76072899f54179e674d9f2e5
bianling <1343881168@qq.com>
2022-07-07 17:12:58 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'https://www.escook.cn/'
+Vue.prototype.$axios = axios
+
 
 Vue.config.productionTip = false
 
